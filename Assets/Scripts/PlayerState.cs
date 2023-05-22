c7c20a09f1f392f21ff0f85fff55f5bec9880920
Ghost <marshalonah@gmail.com>
2023-05-22 13:52:25 +0100

    StateMaching implementedd

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Player player;
+    protected PlayerStateMachine stateMachine;
+    private string animBool;
+
+    public PlayerState(Player player, PlayerStateMachine stateMachine, string animBool)
     {
-        
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.animBool = animBool;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Enter()
     {
-        
+
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
     }
 }
